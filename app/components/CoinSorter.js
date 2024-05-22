3a893b0294b92f496f3fc3149df0f8e315223ab3
midCenturyDude7 <mgriffes@gmail.com>
2024-05-21 17:10:21 -0700

    Setting up workflow

---
+++ app/components/CoinSorter.js
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+import range from "lodash/range";
+import { motion } from "framer-motion";
+
+import styles from "./CoinSorter.module.css"
+
+export default function CoinSorter() {
+  return <div>CoinSorter</div>;
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import CoinSorter from "./components/CoinSorter";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+    <div className="flex min-h-screen items-center justify-center">
+      <CoinSorter />
+    </div>
   );
 }
