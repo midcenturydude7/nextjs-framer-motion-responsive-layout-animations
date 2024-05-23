f2feb3f37e4c83cf8dfbbd7ec4a2831d70285ce1
midCenturyDude7 <mgriffes@gmail.com>
2024-05-22 20:34:26 -0700

    Minor edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      animation: {
-        tilt: "tilt 10s infinite linear",
-      },
-      keyframes: {
-        tilt: {
-          "0%, 50%, 100%": {
-            transform: "rotate(0deg)",
-          },
-          "25%": {
-            transform: "rotate(1.5deg)",
-          },
-          "75%": {
-            transform: "rotate(-1.5deg)",
-          },
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    },
+    animation: {
+      tilt: "tilt 10s infinite linear",
+    },
+    keyframes: {
+      tilt: {
+        "0%, 50%, 100%": {
+          transform: "rotate(0deg)",
+        },
+        "25%": {
+          transform: "rotate(1.5deg)",
+        },
+        "75%": {
+          transform: "rotate(-1.5deg)",
         },
       },
     },
   },
-  plugins: [],
 };
+export const plugins = [];
